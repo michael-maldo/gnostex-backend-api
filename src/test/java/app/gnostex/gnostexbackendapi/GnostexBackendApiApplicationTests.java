4bd2f357d7146ef1170c0f4f4bef3257b7bed6cd
gnosticka <gnosticka@gmail.com>
2025-12-19 12:20:47 +1100

    init spring boot

---
+++ src/test/java/app/gnostex/gnostexbackendapi/GnostexBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.gnostex.gnostexbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnostexBackendApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
