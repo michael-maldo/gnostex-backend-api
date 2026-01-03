424da670cabe62ea15b1bef7d16e7bf8985b63a6
gnosticka <gnosticka@gmail.com>
2026-01-04 03:05:53 +1100

    CI - workflow - GHCR - deploy

--- src/test/java/app/gnostex/gnostexbackendapi/ContextLoadsTest.java
+++ src/test/java/app/gnostex/gnostexbackendapi/ContextLoadsTest.java
@@ -1,6 +1,8 @@
 package app.gnostex.gnostexbackendapi;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -9,19 +11,30 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @SpringBootTest
+@ActiveProfiles("test") // points to application-test.yml if needed
 class ContextLoadsTest {
 
+    // Use @SuppressWarnings to silence the "try-with-resources" warning
+    @SuppressWarnings("resource")
     @Container
-    static PostgreSQLContainer<?> postgres =
+    private static final PostgreSQLContainer<?> POSTGRES_CONTAINER =
             new PostgreSQLContainer<>("postgres:16")
                     .withDatabaseName("testdb")
                     .withUsername("test")
                     .withPassword("test");
 
+    // Dynamically inject container properties into Spring Boot
     @DynamicPropertySource
-    static void overrideProps(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRES_CONTAINER::getPassword);
+        registry.add("spring.datasource.driver-class-name", POSTGRES_CONTAINER::getDriverClassName);
+    }
+
+    @Test
+    void contextLoads() {
+        // This test will start Spring context + Postgres container
+        // and verify that the application context loads successfully
     }
 }
