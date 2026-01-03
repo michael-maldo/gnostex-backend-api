2b74e341467c5e9d7e82091c2eb4a3a1930795dd
gnosticka <gnosticka@gmail.com>
2026-01-04 02:15:47 +1100

    CI - workflow - GHCR - deploy

--- src/test/java/app/gnostex/gnostexbackendapi/GnostexBackendApiApplicationTests.java
+++ src/test/java/app/gnostex/gnostexbackendapi/GnostexBackendApiApplicationTests.java
@@ -2,12 +2,10 @@ package app.gnostex.gnostexbackendapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(
-        properties = {
-                "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
-        }
-)
+@SpringBootTest
+@ActiveProfiles("test")
 class GnostexBackendApiApplicationTests {
 
     @Test
